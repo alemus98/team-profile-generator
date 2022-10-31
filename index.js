113c41422e1de632d226706b0937c1a239c13fb9
Andy L <andresslemus3@gmail.com>
2022-10-31 01:56:47 -0700

    added code for index.js

---
+++ index.js
@@ -0,0 +1,39 @@
+const inquirer = require('inquirer');
+const createPage = require('./src/createPage');
+const renderPage = require('./src/renderHTML');
+const employeeJobs = require('./lib/roles');
+
+// prompta user input and log prompt questions from ./libs
+const getEmployeeDataAsync = async () => {
+    const employeeList = [];
+
+    while (true) {
+        // prompts user for selection of role
+        const response = await inquirer.prompt({
+            type: 'list',
+            name: 'role', 
+            message: 'Please select role to add:',
+            choices: ['Senior Developer', 'Junior Developer', 'Intern', 'Exit']
+        });
+
+        const employeeJob = response.role;
+        if (employeeJob === 'Exit') break;
+
+  
+        const employeeConstructor = employeeJobs[employeeJob];
+        const employeeInfo = await inquirer.prompt((new employeeConstructor).getPromptQuestions());
+
+        console.log(`\nNew ${employeeJob} profile created for ${employeeInfo.name}.\n`);
+
+        employeeList.push({
+            role: employeeJob,
+            ...employeeInfo
+        });
+    }
+
+    return employeeList;
+}
+
+getEmployeeDataAsync()
+    .then(createPage)
+    .then(renderPage);
