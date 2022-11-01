b2cc7337b54780c7682c317902b6fce710ba8d4a
Andy L <andresslemus3@gmail.com>
2022-11-01 00:57:20 -0700

    edited engineer.test

--- tests/engineer.test.js
+++ tests/engineer.test.js
@@ -1,20 +1,19 @@
-const Engineer = require('../lib/Engineer');
+const Engineer = require("../lib/Engineer");
 
-test('Engineer constructor arguments should log', () => {
-    const engineer = new Engineer('https://github.com/jonwong2020', 'Jon', 1, 'Jon@gmail.com');
+test("Can set GitHUb account via constructor", () => {
+  const testValue = "GitHubUser";
+  const e = new Engineer("Lilith", 1, "test@test.com", testValue);
+  expect(e.github).toBe(testValue);
+});
 
-    expect(engineer.github).toBe('https://github.com/jonwong2020');
-    expect(engineer.name).toBe('Jon');
-    expect(engineer.id).toEqual(expect.any(Number));
-    expect(engineer.email).toBe('Jon@gmail.com');
-})
+test("getRole() should return \"Engineer\"", () => {
+  const testValue = "Engineer";
+  const e = new Engineer("Lilith", 1, "test@test.com", "GitHubUser");
+  expect(e.getRole()).toBe(testValue);
+});
 
-test('Engineer method results', () => {
-    const engineer = new Engineer('https://github.com/jonwong2020', 'Jon', 2, 'Jon@gmail.com');
-
-    expect(engineer.getGitHub()).toBe('https://github.com/jonwong2020');
-    expect(engineer.getName()).toBe('Jon');
-    expect(engineer.getId()).toBe(2);
-    expect(engineer.getEmail()).toBe('Jon@gmail.com');
-    expect(engineer.getRole()).toBe('Engineer')
-})
+test("Can get GitHub username via getGithub()", () => {
+  const testValue = "GitHubUser";
+  const e = new Engineer("Lilith", 1, "test@test.com", testValue);
+  expect(e.getGithub()).toBe(testValue);
+});
